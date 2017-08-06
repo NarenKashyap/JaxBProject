f3024a6f0742b91c109172cd12fb48dd43a52cd9
NarenKashyap <30664221+NarenKashyap@users.noreply.github.com>
2017-08-06 21:38:47 +0530

    added else case

--- JaxBProject/src/com/java8/lembda/TestLembda.java
+++ JaxBProject/src/com/java8/lembda/TestLembda.java
@@ -9,25 +9,26 @@ public class TestLembda {
 
 	public static void main(String[] args) {
 		System.out.println("Start Lembda Project");
-		
+
 		String date = "2017-04-28 10:04:32";
-		DateTimeZone timeZoneLondon = DateTimeZone.forID( "Europe/London" );
-		DateTimeZone timeZoneGMT = DateTimeZone.forID( "Etc/Greenwich" );
+		DateTimeZone timeZoneLondon = DateTimeZone.forID("Europe/London");
+		DateTimeZone timeZoneGMT = DateTimeZone.forID("Etc/Greenwich");
 		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 		DateTime dt1 = formatter.parseDateTime(date);
 		System.out.println(DateTimeZone.getDefault().getID());
 		System.out.println(dt1);
-		
-		
-		DateTime nowLondon = DateTime.now( timeZoneLondon );
-		DateTime nowGMT = nowLondon.withZone( timeZoneGMT );
-		//DateTime nowUtc = nowLondon.withZone( DateTimeZone.UTC );
+
+		DateTime nowLondon = DateTime.now(timeZoneLondon);
+		DateTime nowGMT = nowLondon.withZone(timeZoneGMT);
+		// DateTime nowUtc = nowLondon.withZone( DateTimeZone.UTC );
 		System.out.println(nowGMT);
 		System.out.println(nowLondon);
-		
-		if(dt1.isBefore(nowLondon.getMillis())){
-			System.out.println("equals");
+
+		if (dt1.isBefore(nowLondon.getMillis())) {
+			System.out.println("Equals");
+		} else {
+			System.out.println("Not Equal");
 		}
-		
+
 	}
 }
