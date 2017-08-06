00b5f0f51057c42b03a238bbda5443480ab18301
NarenKashyap <30664221+NarenKashyap@users.noreply.github.com>
2017-08-06 19:27:50 +0530

    msg 3333

--- JaxBProject/src/com/java8/lembda/TestLembda.java
+++ JaxBProject/src/com/java8/lembda/TestLembda.java
@@ -8,7 +8,8 @@ import org.joda.time.format.DateTimeFormatter;
 public class TestLembda {
 
 	public static void main(String[] args) {
-		System.out.println("Start Lembda Project 2222 3333");
+		System.out.println("Start Lembda Project 2222");
+		System.out.println("Start Lembda Project 3333");
 		
 		String date = "2017-04-28 10:04:32";
 		DateTimeZone timeZoneLondon = DateTimeZone.forID( "Europe/London" );
