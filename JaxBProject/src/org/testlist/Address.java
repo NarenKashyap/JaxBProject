8c0e6c2b467deddcb14cc6b22ce4efbba1171a5d
NarenKashyap <30664221+NarenKashyap@users.noreply.github.com>
2017-08-06 22:46:41 +0530

    committed once again

--- JaxBProject/src/org/testlist/Address.java
+++ JaxBProject/src/org/testlist/Address.java
@@ -2,5 +2,21 @@ package org.testlist;
 
 public class Address {
 	
+	private String name;
+	private String city;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
 
 }
