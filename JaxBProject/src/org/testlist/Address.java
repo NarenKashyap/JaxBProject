02cdc9e394c38afa9ffa14cfc4e409f9b2971022
NarenKashyap <30664221+NarenKashyap@users.noreply.github.com>
2017-08-08 22:41:04 +0530

    Update Address.java
    require to add an id

--- JaxBProject/src/org/testlist/Address.java
+++ JaxBProject/src/org/testlist/Address.java
@@ -1,7 +1,7 @@
 package org.testlist;
 
 public class Address {
-	
+	// require to add an id
 	private String name;// set name
 	private String city;//set city-- city is modified -- modified again due to country
 	private String country; // set country again
